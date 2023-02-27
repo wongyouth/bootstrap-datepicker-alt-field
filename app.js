166cd1cad48efcad382574cf80d7d5c38fdf0136
Ryan Wang <wongyouth@gmail.com>
2023-02-27 14:27:52 +0800

    update app.js
    update setDate method.

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ $(function () {
 
   el.datepicker({
     format: "yyyy年mm月dd日",
-    todayHighlight: true,
     language: "zh-CN",
+    todayHighlight: true,
     todayBtn: true
-  }).on("changeDate", (e) => {
-    dateEl.val(e.format("yyyy-mm-dd"));
   });
 
+  // add alt field for datepicker input
+
   var name = el.attr("name");
+  // var dateEl = $(`<input type="hidden" name="${name}">`);
   var dateEl = $(`<input type="text" name="${name}">`);
+  el.on("changeDate", (e) => {
+    dateEl.val(e.format("yyyy-mm-dd"));
+  });
   el.parent().append(dateEl);
-  el.data("datepicker").setDate(el.val());
+  el.datepicker("setDate", el.val());
 });
