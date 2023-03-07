78fa53fc8a5a62d24b43b12a6d0d1591dd782b42
Ryan Wang <wongyouth@gmail.com>
2023-03-07 16:38:23 +0800

    Update 3 files (#4)

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
 $(function () {
-  $("input.datepicker").each((_, item) => {
-    var el = $(item);
-
-    // add alt field for datepicker input
-
-    var name = el.attr("name");
-    // var dateEl = $(`<input type="hidden" name="${name}">`);
-    var dateEl = $(`<input type="text" name="${name}">`);
-
-    el.datepicker({
-      format: "yyyy年mm月dd日",
-      language: "zh-CN",
-    }).on("changeDate", (e) => {
-      dateEl.val(e.format("yyyy-mm-dd"));
-    });
-
-    el.datepicker("setDate", el.val());
-
-    el.parent().append(dateEl);
+  $("input.datepicker").datepicker({
+    uniformFormat: "mm-dd-yyyy",
   });
 });
